2755725a8aa68a0669a223b5cd3b5a38c7b989a8
Akshat Jain <jnakshat202@gmail.com>
2025-04-21 18:56:57 +0530

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,11 +16,10 @@ const Hero = () => {
 
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
-            Hi, I'm <span className='text-[#915EFF]'>Adrian</span>
+            Hi, I'm <span className='text-[#915EFF]'>Akshat</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
-            interfaces and web applications
+            I develop and build really boring stuff. But quite often i stumble upon some really cool things, and thse are displayed here !!
           </p>
         </div>
       </div>
